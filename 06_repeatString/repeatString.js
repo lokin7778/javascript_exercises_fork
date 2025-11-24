6ee2bd53979b7ea16b35edf541e23c897e7cd360
Lokesh <lokin7778@gmail.com>
2025-11-24 12:35:03 -0500

    Used the for-loop to return the required repeated string and used the if-else statements to check if the user enters a negative numbers

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    let repeatedString = ""; // declare an empty string
+
+    if ( num < 0){
+        return "ERROR";
+    }
+
+    else{
+
+        for (let i = 0; i < num; i++){
+
+            repeatedString = repeatedString + str;
+        }
+
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
