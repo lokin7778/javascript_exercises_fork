28ca6dba1e6be6e4f5014f26eeac78255d2becac
Lokesh <lokin7778@gmail.com>
2025-11-24 13:39:51 -0500

    Final changes committed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+
+  let tempInCalsuius = (tempInFarenheit-32) * (5/9);
+  return Number(tempInCalsuius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCalsuius) {
+
+  let tempInFarenheit = ((9/5)*tempInCalsuius) + 32;
+  return Number(tempInFarenheit.toFixed(1));
+
 };
 
 // Do not edit below this line
